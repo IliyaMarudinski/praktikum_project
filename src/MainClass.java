f6c5877b3d1c4c2ee7fbf22258af81b1725458ac
IliyaMarudinski <57772673+IliyaMarudinski@users.noreply.github.com>
2020-01-28 14:15:51 +0200

    project is ready to go

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,22 +1,33 @@
+import java.util.Scanner;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int UserCount = 30;
+		int UserCount = 0;
+		Scanner scan = new Scanner(System.in);
 		CustomerList custList = new CustomerList();
-		System.out.println("This is the star point of the main class");
+		System.out.println(" ---------------------- This is the start point of the main class ---------------------- ");
+		while(UserCount < 1 || UserCount > 5000) {
+			System.out.print("TASK 1: Input user count beetwen 1 and 5000: ");
+			if(scan.hasNextInt()) {
+				UserCount = scan.nextInt();
+			}
+			scan.nextLine();
+		}
+		scan.close();
 
 		for(int i = 0; i < UserCount; i++) {
 			custList.addCustomer(UserGenerator.genUser());			
 		}
-		System.out.println("22222222222");	
+		
+		System.out.println("\n\n---------------------- List all users ---------------------- ");	
 		custList.printAll();
-		System.out.println("33333333333");
+		System.out.println("\n\nTASK 2: Sort by name and print");
 		custList.sortByNamePrint(false, 0);
-		System.out.println("44444444444");
+		System.out.println("\n\nTASK 3: Filter 2 rate users sort by Time Spent and print");
 		custList.sortByNamePrint(true, 2);
-		System.out.println("55555555555");
+		System.out.println("\n\nTASK 4: Show register users per year");
 		custList.showCustCountPerYearByRate(2);
 	}
 }
